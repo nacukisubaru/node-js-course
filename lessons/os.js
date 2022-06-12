90d7d64208f81fd0e770b8d71e86e295a3104303
nacukisubaru <alexitsheff@gmail.com>
2022-06-12 19:38:59 +0300

    создание дочерних процессов работа с cluster , os

---
+++ lessons/os.js
@@ -0,0 +1,22 @@
+const cluster = require('cluster');
+const os = require('os');
+
+//console.log(os.platform());
+//console.log(os.arch());
+//количество потоков проца
+//console.log(os.cpus().length);
+
+const cpus = os.cpus();
+//главный процесс
+if(cluster.isPrimary) {
+    //создание дочерних процессов
+    for(let i = 0; i < cpus.length -2; i++) {
+        cluster.fork()
+    }
+} else {
+    console.log(`Воркер с pid = ${process.pid} запущен`);
+    
+    setInterval(()=>{
+        console.log(`Воркер с pid = ${process.pid} ещё работает`);
+    }, 5000);
+}
