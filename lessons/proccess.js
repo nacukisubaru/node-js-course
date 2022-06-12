6f8c0c502edffc9963aac17bf100947991c47883
nacukisubaru <alexitsheff@gmail.com>
2022-06-12 17:27:17 +0300

    enviroment variables

--- lessons/proccess.js
+++ lessons/proccess.js
@@ -1,3 +1,6 @@
+//dotenv для чтения .env файла получения пременных окружения и их инициализация в env файле 
+//либо можно прописать "cross-env PORT=5000 NODE_ENV=prod node ./lessons/proccess.js" в package.json и не использовать dotenv
+//либы cross-env, dotenv
 const dotenv = require('dotenv');
 dotenv.config();
 
